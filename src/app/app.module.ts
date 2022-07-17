2311403e235c10d55f9bfdf9c0c12cbc9578b462
Lautaro Ramadán <ramadanlautar0@hotmail.com>
2022-07-16 22:52:00 -0300

    forms-ramadan v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioComentariosComponent } from './components/formulario-comentarios/formulario-comentarios/formulario-comentarios.component';
+import { FormularioComentariosComponent } from './components/formulario-comentarios/formulario-comentarios.component';
 
 @NgModule({
   declarations: [
--- src/app/components/formulario-comentarios/formulario-comentarios.component.ts
+++ src/app/components/formulario-comentarios/formulario-comentarios.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Comentario } from '../../../classes/Comentario';
+import { Comentario } from '../../classes/Comentario';
 
 @Component({
   selector: 'app-formulario-comentarios',
